1f58bb001569a50871d41f0eb86ade40394586a8
MakcSan16 <118685436+MakcSan16@users.noreply.github.com>
2023-06-10 15:27:39 +0300

    Add files via upload

---
+++ smuzi/1 week/3.py
@@ -0,0 +1,6 @@
+a = int(input("Введите число a="))
+b = int(input("Введите число b="))
+c = int(input("Введите число c="))
+
+print(c-=ax+b)
+
---
+++ smuzi/1 week/5.py
@@ -0,0 +1,8 @@
+V = float(input("Объем="))
+m = float(input("Масса тела="))
+p = m/V
+
+print ("Плотность материала =",(round(p,2)))
+
+
+
